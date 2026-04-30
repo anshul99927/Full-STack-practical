e26d582c1349b7f3a95dd318bdd5c5de9414e8da
BADAL SINGH <anshul9992747116@gmail.com>
2026-04-30 15:16:39 +0530

    Create middleware.js

---
+++ middleware.js
@@ -0,0 +1,11 @@
+
+
+// Middleware: check if user is logged in
+module.exports.isLoggedIn = (req, res, next) => {
+  if (!req.isAuthenticated()) {
+    req.session.returnTo = req.originalUrl;
+    req.flash("error", "You must be logged in to do that!");
+    return res.redirect("/login");
+  }
+  next();
+};
